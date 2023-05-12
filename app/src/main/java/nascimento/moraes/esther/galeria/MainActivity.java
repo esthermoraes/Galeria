bf3bc89b74ef70df06b371e749ea31374ef942dd
20211tiimi0052 <esther.ifes2021@gmail.com>
2023-05-12 08:58:56 -0300

    APP 5 => Galeria (Página 25, passo 12)

--- app/src/main/java/nascimento/moraes/esther/galeria/MainActivity.java
+++ app/src/main/java/nascimento/moraes/esther/galeria/MainActivity.java
@@ -1,15 +1,27 @@
 package nascimento.moraes.esther.galeria;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    static int RESULT_TAKE_PICTURE = 1;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +44,60 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()) {
             case R.id.opCamera:
-               dispatchTakePictureIntent();
-               return true;
+                dispatchTakePictureIntent();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
-            }
+        }
+    }
+
+    public void startPhotoActivity(String photoPath) {
+        Intent i = new Intent(MainActivity.this, PhotoActivity.class);
+        i.putExtra("photo_path", photoPath);
+        startActivity(i);
+    }
+
+    private void dispatchTakePictureIntent() {
+        File f = null;
+        try {
+            f = createImageFile();
+        } catch (IOException e) {
+            Toast.makeText(MainActivity.this, "Não foi possível criar o arquivo", Toast.LENGTH_LONG).show();
+            return;
         }
 
+        currentPhotoPath = f.getAbsolutePath();
 
+        if (f != null) {
+            Uri fUri = FileProvider.getUriForFile(MainActivity.this, "trindade.daniel.galeria.fileprovider", f);
+            Intent i = new
+                    Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            i.putExtra(MediaStore.EXTRA_OUTPUT, fUri);
+            startActivityForResult(i, RESULT_TAKE_PICTURE);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File f = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return f;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_TAKE_PICTURE) {
+            if (resultCode == Activity.RESULT_OK) {
+                photos.add(currentPhotoPath);
+                mainAdapter.notifyItemInserted(photos.size() - 1);
+            } else {
+                File f = new File(currentPhotoPath);
+                f.delete();
+            }
+        }
+    }
 }
+
+
--- app/src/main/java/nascimento/moraes/esther/galeria/PhotoActivity.java
+++ app/src/main/java/nascimento/moraes/esther/galeria/PhotoActivity.java
@@ -1,11 +1,16 @@
 package nascimento.moraes.esther.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 public class PhotoActivity extends AppCompatActivity {
@@ -21,6 +26,17 @@ public class PhotoActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
+    String photoPath;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo);
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+        Bitmap bitmap = Utils.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -28,5 +44,16 @@ public class PhotoActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.photo_activity_tb, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.opShare:
+                sharePhoto();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
